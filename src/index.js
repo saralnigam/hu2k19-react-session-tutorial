dbc4dae07e79956c9bdcfc952c0bcbc454e6c1a0
saralnigam <saral.nigam@hashedin.com>
2019-01-17 14:42:29 +0530

    Step 4 Creating Class component and usage

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Import React Components
+import App from './App';
+
 import './index.css';
 
 // Grab the DOM element
 const rootElement = document.getElementById('root');
 
-// Create your React Component
-function Heading(){
-  return <h1>Hello World</h1>;
-}
-
 // This is point of interaction of React and HTML DOM
 // Add your React Element for rendering
-ReactDOM.render(Heading(), rootElement);
-// ReactDOM.render(<Heading />, rootElement);
+ReactDOM.render(<App />, rootElement);
