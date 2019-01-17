a3dbc984e888baf051f116f1c1e4721cccb44276
saralnigam <saral.nigam@hashedin.com>
2019-01-17 13:40:21 +0530

    Step 3 Added functional component

--- src/index.js
+++ src/index.js
@@ -7,13 +7,12 @@ import './index.css';
 // Grab the DOM element
 const rootElement = document.getElementById('root');
 
-// Create your React element
-const reactElement = React.createElement(
-  "h1",
-  {},
-  "Hello World"
-)
+// Create your React Component
+function Heading(){
+  return <h1>Hello World</h1>;
+}
 
 // This is point of interaction of React and HTML DOM
 // Add your React Element for rendering
-ReactDOM.render(reactElement, rootElement);
+ReactDOM.render(Heading(), rootElement);
+// ReactDOM.render(<Heading />, rootElement);
