1a7ab3de280f6de2540d99e56a9e860214cea1af
saralnigam <saral.nigam@hashedin.com>
2019-01-17 14:47:54 +0530

    Step 5 Intrduction to props and component composition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // Import React Dependencies
 import React, { Component } from 'react';
 
+// Import Components
+import Heading from './components/Heading/Heading';
+
 // Import Styles
 import './App.css';
 
@@ -8,7 +11,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <Heading headingText="Hello World!!" />
     );
   }
 }
