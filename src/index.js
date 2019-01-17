1aa056a1f434b92aaf588d65eef34bba153abbc8
saralnigam <saral.nigam@hashedin.com>
2019-01-17 13:21:10 +0530

    Step 1 Updating DOM via JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Grab the DOM element
+const rootElement = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Create your DOM element
+const headingElement = document.createElement('h1');
+headingElement.innerHTML = "Hello World"
+
+// Add your DOM element to the DOM for rendering
+rootElement.appendChild(headingElement);
